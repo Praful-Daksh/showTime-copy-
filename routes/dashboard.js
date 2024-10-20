823d865178460473c0f32570b3cb4c2f28463b10
Praful-Daksh <prafuldaksh@proton.me>
2024-10-20 22:03:10 +0530

    all events section

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,7 +5,38 @@ import { ensureAuthenticated } from "../config/auth.js";
 const router = express.Router();
 
 // Route for dashboard home
+
 router.get("/home", ensureAuthenticated, async (req, res) => {
+    try {
+        let e = [];
+        let userId = req.user._id;
+        const currentDate = new Date();
+        await Event.find({ user: userId, date: { $gte: currentDate } })
+            .then(events => {
+                e = events;
+            })
+            .catch(err => {
+                console.error('Error fetching events:', err);
+            });
+        res.render("dashboard", {
+            layout: "dashboard-layout.ejs",
+            cssFile: "dashboard.css",
+            user: req.user,
+            E: e,
+            eventLength: e.length,
+        });
+
+    } catch (error) {
+        if (error) {
+            console.log(error);
+            res.send("Service Unavailable (502)");
+        }
+    }
+});
+
+//route for the events section
+
+router.get("/AllEvents", ensureAuthenticated, async (req, res) => {
     try {
         let e = [];
         let userId = req.user._id;
@@ -16,21 +47,47 @@ router.get("/home", ensureAuthenticated, async (req, res) => {
             .catch(err => {
                 console.error('Error fetching events:', err);
             });
-            res.render("dashboard", {
-                layout: "dashboard-layout.ejs",
-                cssFile: "dashboard.css",
-                user: req.user,
-                E:e,
-                eventLength:e.length,
-            });
-        
+        res.render("dash-events", {
+            layout: "dashboard-layout.ejs",
+            cssFile: "dashboard.css",
+            E: e,
+        });
+
     } catch (error) {
         if (error) {
             console.log(error);
-            res.send("Server Overloaded , Try again later");
+            res.send("Internal Server Error , 502");
+            res.render('/login');
         }
     }
 });
+
+// route for viewing an event  
+
+router.get('/AllEvents/:id', ensureAuthenticated, async (req, res) => {
+    try {
+        const eventId = req.params.id;
+        await Event.findById(eventId)
+            .then(event => {
+                res.render('viewEvent', {
+                    layout: "dashboard-layout.ejs",
+                    cssFile: "dashboard.css",
+                    E: event,
+                });
+            })
+            .catch(er => {
+                res.send("Event doesn't Exist Anymore");
+            })
+    }
+    catch (err) {
+        if (err) {
+            console.log(error);
+            res.send("Internal Server Error , 502");
+            res.render('/dashboard/AllEvents');
+        }
+    }
+});
+
 router.get("/settings", ensureAuthenticated, (req, res) => {
     res.render("dash-settings", { layout: "dashboard-layout.ejs" });
 });
@@ -57,7 +114,7 @@ router.post("/newEvent", async (req, res) => {
             venue,
         });
         await newEvent.save();
-        res.redirect("/dashboard/home");
+        res.redirect("/dashboard/AllEvents");
         console.log("New Event Created", newEvent);
     } catch (error) {
         console.log("error in creating event", error);
